e8468a2b9ac08443ec1b8b8acba0fc197c5e188c
Yuvraj <rathva.1@iitj.ac.in>
2024-10-17 12:11:51 +0530

    Added dashboard in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { AppProvider } from "@toolpad/core";
+import { DashboardLayout } from "@toolpad/core/DashboardLayout";
+import { PageContainer } from "@toolpad/core/PageContainer";
+import { Key, Hub } from "@mui/icons-material";
+
+export const NAVIGATION = [
+  {
+    segment: "credentials",
+    title: "Credentials",
+    icon: <Key />,
+  },
+];
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <AppProvider
+      navigation={NAVIGATION}
+      branding={{
+        logo: <Hub fontSize="large" style={{ padding: "5px" }} />,
+        title: "SSO IITJ",
+      }}
+    >
+      <DashboardLayout>
+        <PageContainer>
+          <h1>Home</h1>
+        </PageContainer>
+      </DashboardLayout>
+    </AppProvider>
   );
 }
