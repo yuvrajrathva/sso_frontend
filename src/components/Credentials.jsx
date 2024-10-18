e836110b57534f0d8c9487a5f9dec8f46216d566
Yuvraj <rathva.1@iitj.ac.in>
2024-10-18 14:34:11 +0530

    Added key management table on credentials page

--- src/components/Credentials.jsx
+++ src/components/Credentials.jsx
@@ -1,15 +1,40 @@
+import React from "react";
+import { Box, Button } from "@mui/material";
+import { Add } from "@mui/icons-material";
+import KeysTable from "./KeysTable";
+
 export default function Credentials() {
+  const [rows, setRows] = React.useState([{
+    id: 1,
+    name: "Default",
+    client_id: "123456",
+    client_secret: "abcdef",
+    created_at: "2021-10-01",
+    scopes: ["name", "email"],
+  },
+  {
+    id: 2,
+    name: "Test",
+    client_id: "654321",
+    client_secret: "fedfasf",
+    created_at: "2021-10-02",
+    scopes: ["name", "email", "phone"],
+  }]);
+
   return (
-    <div>
-      <h1>API Keys</h1>
+    <>
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        <h1>API Keys</h1>
+        <Button variant="text" sx={{ fontWeight: "bold" }}>
+          <Add />
+          CREATE CREDENTIALS
+        </Button>
+      </Box>
       <p>
-        API keys are used to authenticate requests to the API. You can generate
-        and manage your API keys in your account settings.
+        API keys are used to authorize your app's requests to the sso. You can
+        generate and manage your API keys in your account settings.
       </p>
-      <h3>CLIENT ID</h3>
-      <p>33daf2bf-5583-4137-80bf-389abfdad32f</p>
-      <h3>CLIENT SECRET</h3>
-      <p>dwkfhasf yyhifasdfsaf hihfiasdhffddd</p>
-    </div>
+      <KeysTable rows={rows} />
+    </>
   );
 }
