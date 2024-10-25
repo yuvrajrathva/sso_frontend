c9fe0d457fc66442f4a8eea78495191e8ea5329f
Yuvraj <rathva.1@iitj.ac.in>
2024-10-25 18:59:02 +0530

    Adding Setup page

---
+++ src/components/SetupPage.jsx
@@ -0,0 +1,8 @@
+export default function SetupPage() {
+  return (
+    <>
+      <h1>Setup Page</h1>
+      <p>This is the setup page. You can use this page to set up your app.</p>
+    </>
+  );
+}
